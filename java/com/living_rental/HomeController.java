b5b6f149a21abdeacc5af93ba82dd4bebfd9f2d4
asthu <osm0348@naver.com>
2021-11-08 16:03:52 +0900

    first commit

---
+++ java/com/living_rental/HomeController.java
@@ -0,0 +1,17 @@
+package com.living_rental;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+}
